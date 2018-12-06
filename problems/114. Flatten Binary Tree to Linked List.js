d3d01c62ebf20b88a0012ccb8f50fa304029ede5
jin5354 <xiaoyanjinx@gmail.com>
2018-12-06 20:33:19 +0800

    [doc] add medium.114

---
+++ problems/114. Flatten Binary Tree to Linked List.js
@@ -0,0 +1,57 @@
+// Given a binary tree, flatten it to a linked list in-place.
+
+// For example, given the following tree:
+
+//     1
+//    / \
+//   2   5
+//  / \   \
+// 3   4   6
+// The flattened tree should look like:
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+//          \
+//           6
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+  let pointer = null
+
+  function walk(node) {
+    if(!node) {
+      return null
+    }
+
+    if(pointer) {
+      pointer.right = node
+    }
+
+    pointer = node
+
+    let right = node.right
+    walk(node.left)
+    node.left = null
+    walk(right)
+  }
+
+  walk(root)
+};
